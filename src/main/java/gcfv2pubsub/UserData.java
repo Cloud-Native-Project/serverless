05d18db65de108c7a3b87948b7f90166aad6157c
Sandesh Reddy Tummala <tummala.san@northeastern.edu>
2024-03-27 17:29:39 -0400

    update

---
+++ src/main/java/gcfv2pubsub/UserData.java
@@ -0,0 +1,23 @@
+package gcfv2pubsub;
+
+class UserData {
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String username;
+    private String account_created;
+    private String account_updated;
+
+    // Getters and Setters for these fields
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Include getters and setters for other fields if necessary
+}
+
